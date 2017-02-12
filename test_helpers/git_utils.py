60831e88925a27e4a2fa4a0713c38f29473fbe0f
Charles Sanquer <charles.sanquer@gmail.com>
2017-02-12 13:31:39 +0100

    add docstrings

--- test_helpers/git_utils.py
+++ test_helpers/git_utils.py
@@ -8,6 +8,9 @@ import os
 from git import Repo
 
 def commit(repo, message, author=None, date=None):
+    '''
+    create a Commit
+    '''
     cwd = os.getcwd()
 
     os.chdir(repo.working_dir)
@@ -26,18 +29,30 @@ def commit(repo, message, author=None, date=None):
     return repo.commit('HEAD')
 
 def tag(repo, version, author=None, date=None):
+    '''
+    create a Commit and a Tag on this commit
+    '''
     commit_obj = commit(repo=repo, message="release: {}".format(version), author=author, date=date)
     repo.create_tag(path=version, message=version)
 
     return commit_obj
 
 def branch(repo, name, start='HEAD'):
+    '''
+    create a Branch
+    '''
     return repo.create_head(name, commit=start)
 
 def clone(remote_repo, path):
+    '''
+    clone a git repository
+    '''
     return remote_repo.clone(path)
 
 def init(email='user@example.com', username='User Test', repo_dir=None):
+    '''
+    init a empty git repository and set basic config
+    '''
     cwd = os.getcwd()
     if not repo_dir or not os.path.exists(repo_dir):
         repo_dir = cwd
@@ -63,6 +78,9 @@ def default_init(version='0.1.2',
                  author='User Test <user@example.com>',
                  date='2016-11-20T12:41:30+0000',
                  tag_date='2016-11-20T12:42:30+0000', repo_dir=None):
+    '''
+    init a empty git repository with basic config and create 2 commit and 1 tag fixture
+    '''
     repo = init(email=email, username=username, repo_dir=repo_dir)
     commit(repo=repo, message='initial commit', author=author, date=date)
     tag(repo=repo, version=version, author=author, date=tag_date)
